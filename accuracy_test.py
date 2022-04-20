d8f536a5ae1a64055328d2c6854b0acfa1386f37
Matias <64340009+CineraM@users.noreply.github.com>
2022-04-19 23:05:34 -0400

    update
    update

--- accuracy_test.py
+++ accuracy_test.py
@@ -8,13 +8,13 @@ def load_model(filename):
 #load in the trained model
 model = load_model("model.tf")
 
-
+# this is the prediction function that uses the already model and determines the sentiment
 def predict_tweet(tweet, model):
     
     string = str.encode(tweet)                  # format the tweet for the network
     encoded_tweet = [string]
     yfit = model(tf.constant(encoded_tweet))
-    print(yfit)
+    # print(yfit)
     yfit = yfit.numpy().argmax(axis=1)          # fit the tweet on the network
 
     sentiment = ""
@@ -29,40 +29,42 @@ def predict_tweet(tweet, model):
     
     return sentiment
 
-
-train_testing = tf.data.TextLineDataset("emotions_test.csv").skip(1)
+# This is the smaller testing set used to evaluate accuracy
+testing = tf.data.TextLineDataset("emotions_test_small.csv").skip(1)
+# list to hold the tweets
 x_train = []
+# list to hold the sentiments of the tweets in test
 sent_list = []
+# list to hold the predited sentiment by the model
 result_sent = []
+
 i = 0
-for line in train_testing:
+for line in testing:
+    # split the csv
     split_line = tf.strings.split(line, ",", maxsplit=2)
-    x_train.append(split_line[2].numpy())
-    sent_list.append(split_line[1].numpy().decode('UTF-8'))
-    x = x_train[i].decode('UTF-8')
-    result_sent.append(predict_tweet(x, model))
-    i += 1
-    if(i >= 79):
-        break
+    # specify emotion here to get that accuracy
+    # if the line in the dataset is the specified emotion, send the tweet to the model 
+    # and add the actual sentiment and predicted sentiment to respective lists
+    if(split_line[1].numpy().decode('UTF-8') == 'anger'):
+        x_train.append(split_line[2].numpy())
+        sent_list.append(split_line[1].numpy().decode('UTF-8'))
+        x = x_train[i].decode('UTF-8')
+        i += 1
+        result_sent.append(predict_tweet(x, model))
+   
 
 #loop variable to loop through the x_train and count to get accuracy
 k = 0
-amt_correct = 0
+predicted_correct = 0
 #calculate accuracy for the specified emotion
 for sent in sent_list:
     print(sent)
     print ("Result " + result_sent[k])
     if (sent == result_sent[k]):
-        amt_correct += 1
+        predicted_correct += 1
     k += 1
 
-print(amt_correct)
-acc = (amt_correct/len(sent_list))*100
+#calculate the accuracy
+acc = (predicted_correct/len(sent_list))*100
 print(acc)
 print("Accuracy: "+ str(acc))
-
-
-    
-# a = "Just got back from seeing @GaryDelaney in Burslem. AMAZING!! Face still hurts from laughing so much #hilarious"
-
-# print(predict_tweet(a, model))
